cc4252bfdc269b47757b81d9e21fdcb4ff3151c2
brunomlopes <brunomlopes@gmail.com>
2021-01-31 15:48:29 +0000

    Removed commented code

--- tiny85.ino
+++ tiny85.ino
@@ -48,29 +48,6 @@ void setup()
 
 void changed()
 {
-   // int A = digitalRead(pinA);
-   // int B = digitalRead(pinB);
-
-   // int current = (A << 1) | B;
-   // int combined = (previous << 2) | current;
-
-   // if (combined == 0b0010 ||
-   //     combined == 0b1011 ||
-   //     combined == 0b1101 ||
-   //     combined == 0b0100)
-   // {
-   //    counter++;
-   // }
-
-   // if (combined == 0b0001 ||
-   //     combined == 0b0111 ||
-   //     combined == 0b1110 ||
-   //     combined == 0b1000)
-   // {
-   //    counter--;
-   // }
-
-   // previous = current;
 
    int MSB = digitalRead(pinA); //MSB = most significant bit
    int LSB = digitalRead(pinB); //LSB = least significant bit
@@ -103,21 +80,6 @@ void loop()
       previousButtonValue = buttonValue;
    }
    
-   // bool secondButtonValue = digitalRead(pinSecondButton);
-   // Serial.println(secondButtonValue);
-   // if (secondButtonValue != previousSecondButtonValue)
-   // {
-   //    if (secondButtonValue)
-   //    {
-   //       SurfaceDial.press();
-   //    }
-   //    else
-   //    {
-   //       SurfaceDial.release();
-   //    }
-   //    previousSecondButtonValue = secondButtonValue;
-   // }
-
    if (counter >= 1)
    {
       SurfaceDial.rotate(DialRotateDelta);
@@ -129,79 +91,3 @@ void loop()
       counter += 1;
    }
 }
-
-// int RXLED = 17; // The RX LED has a defined Arduino pin
-
-// int encoderPinA = 2;
-// int encoderPinB = 3;
-// int buttonPin = 4;
-
-// volatile int lastEncoded = 0;
-// volatile long encoderValue = 0;
-
-// long lastencoderValue = 0;
-
-// int lastMSB = 0;
-// int lastLSB = 0;
-
-// long readEncoderValue(void)
-// {
-//    return encoderValue / 4;
-// }
-
-// boolean isButtonPushDown(void)
-// {
-//    if (!digitalRead(buttonPin))
-//    {
-//       delay(5);
-//       if (!digitalRead(buttonPin))
-//          return true;
-//    }
-//    return false;
-// }
-
-// void setup()
-// {
-
-//    Serial.begin(9600); //This pipes to the serial monitor
-
-//    pinMode(encoderPinA, INPUT);
-//    pinMode(encoderPinB, INPUT);
-//    pinMode(buttonPin, INPUT);
-
-//    digitalWrite(encoderPinA, HIGH); //turn pullup resistor on
-//    digitalWrite(encoderPinB, HIGH); //turn pullup resistor on
-
-//    //call updateEncoder() when any high/low changed seen
-//    //on interrupt 0 (pin 2), or interrupt 1 (pin 3)
-//    attachInterrupt(digitalPinToInterrupt(encoderPinA), updateEncoder, CHANGE);
-//    attachInterrupt(digitalPinToInterrupt(encoderPinB), updateEncoder, CHANGE);
-// }
-
-// void loop()
-// {
-//    //Do stuff here
-
-//    if (isButtonPushDown())
-//    {
-//       Serial.println("you push button down!!!");
-//    }
-//    Serial.println(readEncoderValue());
-//    delay(50); //just here to slow down the output, and show it will work  even during a delay
-// }
-
-// void updateEncoder()
-// {
-//    int MSB = digitalRead(encoderPinA); //MSB = most significant bit
-//    int LSB = digitalRead(encoderPinB); //LSB = least significant bit
-
-//    int encoded = (MSB << 1) | LSB;         //converting the 2 pin value to single number
-//    int sum = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
-
-//    if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
-//       encoderValue--;
-//    if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000)
-//       encoderValue++;
-
-//    lastEncoded = encoded; //store this value for next time
-// }
